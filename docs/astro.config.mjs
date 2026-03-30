c49ed9d00c08e2831c716933ea138099098ccf1d
Cyberl0rd81 <weyerer@gmx.net>
2026-03-30 08:27:17 +0200

    Update astro.config.mjs

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -2,8 +2,8 @@ import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
 
 export default defineConfig({
-  site: "https://<USERNAME>.github.io",
-  base: "/<REPO-NAME>",
+  site: "https://Cyberl0rd81.github.io",
+  base: "/test",
 
   integrations: [
     starlight({
